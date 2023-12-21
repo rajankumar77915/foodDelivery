14b1dc6d9d48be32e6cb126717140d138d378c67
Gautam kanjariya <100095734+Grkanjariya@users.noreply.github.com>
2023-12-21 09:36:35 +0530

    index.js completed

--- server/index.js
+++ server/index.js
@@ -1,1 +1,39 @@
-<h1>hejje</h1>;
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const restaurantRoutes = require("./routes/resaturantRoute");
+const userRoutes = require("./routes/userRoute");
+
+// express app
+const app = express();
+
+// port
+const port = process.env.PORT || 4000;
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// routes
+app.use("/api/projects", projectsRoutes);
+app.use("/api/user", userRoutes);
+
+// connecting to the database(mongodb)
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listening request
+    app.listen(port, () => {
+      console.log(`Connected to Mongo & Listening on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
