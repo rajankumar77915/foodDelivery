dd33c711b43c0caa867f303052bcc8bc45ea2f82
rajankumar <rajankumar77915@gmail.com>
2023-12-21 09:14:54 +0530

    birthday require false

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 //profile sub-schema for user
 const profileSchema = new mongoose.Schema({
   image: { type: String, required: true },
-  birthdate: { type: Date, required: true },
+  birthdate: { type: Date},
   address: { type: String, required: true },
   pincode: { type: Number, required: true },
 });
